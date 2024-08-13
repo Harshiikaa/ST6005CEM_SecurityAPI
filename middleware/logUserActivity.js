19dae9b89bed6c1e91d372b5a232d4bdb8091107
Harshiikaa <chaudharyharshika8@gmail.com>
2024-08-13 22:56:12 +0545

    cleaned few files

--- middleware/logUserActivity.js
+++ middleware/logUserActivity.js
@@ -15,20 +15,3 @@ const logUserActivity = (req, res, next) => {
 
 module.exports = logUserActivity;
 
-
-// const logger = require('../config/logger');
-
-// const logUserActivity = (req, res, next) => {
-//     const log = {
-//         userName: req.session?.user?.email || 'Guest',
-//         sessionId: req.cookies["connect.sid"] || 'No Session',
-//         url: req.originalUrl,
-//         method: req.method,
-//     };
-
-//     logger.info(log);
-
-//     next();
-// };
-
-// module.exports = logUserActivity;
