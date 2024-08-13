57ed934957efaccb04ac4695d0ab91e065b9e5f9
Harshiikaa <chaudharyharshika8@gmail.com>
2024-08-13 18:39:26 +0545

    changing the console to file for saving

--- config/logger.js
+++ config/logger.js
@@ -6,7 +6,7 @@ const {
 require('winston-mongodb');
 const logger = createLogger({
     transports: [
-        new transports.Console({
+        new transports.File({
             filename: 'info.log',
             level: 'info',
             format: format.combine(format.timestamp(), format.json())
