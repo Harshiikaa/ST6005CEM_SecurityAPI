e072e253d4d52a80bcd985262a81520d5371b631
Harshiikaa <chaudharyharshika8@gmail.com>
2024-08-17 20:20:39 +0545

    edited rating routes

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -1,12 +1,4 @@
-// const router = require('express').Router()
-// const ratingController = require("../controllers/ratingController");
-// const { authGuard } = require('../middleware/authGuard');
 
-// // create rating API
-// router.post('/createRating', authGuard, ratingController.createRating)
-// router.put("/updateRating/:id", authGuard, ratingController.updateRating)
-
-// module.exports = router;
 
 const router = require('express').Router();
 const ratingController = require("../controllers/ratingController");
