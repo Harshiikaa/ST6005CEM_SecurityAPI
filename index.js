d321a10269901bfac2bfa31506106b37cfaccde1
Harshiikaa <chaudharyharshika8@gmail.com>
2024-08-17 20:16:36 +0545

    edited index.js

--- index.js
+++ index.js
@@ -42,12 +42,6 @@ app.use(logUserActivity);
 // dot env config
 dotenv.config();
 
-// const sslServer = https.createServer(
-//   {
-//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-//   }, app)
-// sslServer.listen(3443, () => console.log('secure server on 3443 port'))
 
 const sslOptions = {
   key: fs.readFileSync(path.resolve(__dirname, process.env.SSL_KEY_FILE)),
